39e0c698d6a7232cc5d9b19ee3b6057111744c21
Winnicius-Moura <winnicius.moura@gmail.com>
2022-09-30 04:37:13 -0300

    deploy vercel

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
         sans: ['Inter', 'sans-serif']
       },
       backgroundImage: {
-        galaxy: "url('./background-galaxy.png')",
+        galaxy: "url('./src/assets/background-galaxy.png')",
         logoImg: "url('./src/assets/sl7Logo.png')",
         'sl7-gradient': "linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #22D66D 44.57%)",
       },
