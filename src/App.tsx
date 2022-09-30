0469c2433303c20d89f7690dcba41767462f2fc1
Winnicius-Moura <winnicius.moura@gmail.com>
2022-09-29 23:56:25 -0300

    sl7 teste react: first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './styles/main.css';
+import logoImg from './assets/sl7Logo.png';
+import { Input } from './components/CreateNewInput';
+
+function App(){
+  return (
+    <div className='max-w-[1440px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt="" />
+      <h1 className='text-5xl text-white font-black mt-20 px-4 text-center'>
+        Teste de proficiencia em <span className='text-transparent bg-sl7-gradient bg-clip-text'>react</span>
+      </h1>
+
+      <div className='flex flex-col gap-10 lg:flex-row'>
+        <Input />
+
+      </div>
+      
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CreateNewInput.tsx
@@ -0,0 +1,29 @@
+import { InputHTMLAttributes, useState } from "react"
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function Input(props:InputProps){
+
+    const [text, setText] = useState('')
+    
+    return (
+        <div>
+            <div>
+                <input
+                {...props}
+                value={text}
+                onChange={(inputDigitado) => setText(inputDigitado.target.value)}
+                className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 text-white mt-20 mb-5'
+                placeholder='Digite seu texto aqui: '
+                type='text'
+                name="text"
+                />
+                <div className="w-full h-32 py-3 px-4 bg-zinc-900 text-sm text-white">
+                    <p>
+                        {text} 
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
